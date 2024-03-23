f248f023d277e47ba5bb986148555fa8fda4659b
Alifa Hanania Ardha <alifa.hanania@ui.ac.id>
2024-03-23 10:30:18 +0700

    (5) Multithreaded server using Threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use hello::ThreadPool;
+
 use std::{
     fs,
     io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread, time::Duration,
@@ -5,13 +7,25 @@ use std::{
    
    fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() { let stream = stream.unwrap();
-    handle_connection(stream); }
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
    }
 
    fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+    let request_path = request_line.split_whitespace().nth(1).unwrap();
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"), "GET /sleep HTTP/1.1" => {
@@ -22,9 +36,9 @@ use std::{
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+    
     stream.write_all(response.as_bytes()).unwrap();
-
-   }
+  }
